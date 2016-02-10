7b472ad0b0e0f3f87ae03b5be30778402ff1bdf7
xuewunvs <xuewunvs@gmail.com>
2016-02-10 09:53:04 -0500

    done

--- code/tt3.py
+++ code/tt3.py
@@ -14,18 +14,19 @@ api = TwitterAPI(CONSUMER_KEY,CONSUMER_SECRET,ACCESS_TOKEN_KEY,ACCESS_TOKEN_SECR
 
 
 r = api.request('statuses/filter', {'locations':'-74,40,-73,41'})
-for item in r:		
-#To filter tweets with location information
+for item in r:
 #        if item['place']['name']=='Manhattan' and item['geo']!= None:
-	if item['geo']!= None:
-        	username = item['user']['screen_name']
-        	text = item['text']
-        	timestamp = item['timestamp_ms']
-        	t = int(timestamp[:-3])
-        	attime = time.ctime(t)
-        	place = item['place']['full_name']+", "+item['place']['country']
-        	co = item['coordinates']['coordinates'] #array
-        	print username+ ": " +text+"\n"+attime+"\n"+place+"\n(Coordinates: "+str(co)+")\n"
-       		#print json.dumps(item, indent=1)
-       		stdout.flush()
-       		print "##################################################"
+  if item['geo']!= None:
+    username=item['user']['screen_name']
+    uname= item['user']['name']
+    text = item['text']
+    timestamp = item['timestamp_ms']
+    t = int(timestamp[:-3])
+    attime = time.ctime(t)
+    place = item['place']['full_name']+", "+item['place']['country']
+    co = item['coordinates']['coordinates'] #array
+    strs = uname+" ( "+username +") : " +text+"\n"+attime+"\n"+place+"\n(Coordinates: "+str(co)+")\n"
+    	#print json.dumps(item, indent=1)
+    print str(strs.encode('utf-8'))
+    stdout.flush()
+    print "##################################################"
